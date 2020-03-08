99976069c097894c4646f01605029c16ee0b1a7a
Wibbbs <andrew.wiebe@gmail.com>
2020-03-07 23:30:55 -0500

    Update

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=80)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    published = models.BooleanField()
+    #thumbnail
+    #author
+
+    def __str__(self):
+        return self.title
+
